d4d57d902f01f7b16d47c34768c34a8ff4bdb7fa
tmair32 <tmair32@gmail.com>
2023-06-24 21:23:32 +0900

    Design: Set Up Layout #2

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom'
+import Header from './components/common/Header/header.tsx'
 
 const Layout = () => (
   <>
-    <div className="max-w-[980px] mx-auto">
-      <nav className="bg-red-500 w-full">hihi</nav> // header
-      <div className="bg-blue-500">
+    <Header />
+    <main className="container grid mx-auto grid-cols-4 xl:grid-cols-12 md:grid-cols-8 gap-x-5 h-[calc(100vh-3.5rem)]">
+      <section className="col-span-full md:col-span-4 xl:col-start-5 md:col-start-3 bg-blue-50 h-full p-7">
         <Outlet />
-      </div>
-    </div>
+      </section>
+    </main>
   </>
 )
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import About from './pages/About.tsx'
 import './index.css'
+import Intro from './pages/Intro/index.tsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
+      {
+        path: '/intro',
+        element: <Intro />,
+      },
       {
         path: '/about',
         element: <About />,

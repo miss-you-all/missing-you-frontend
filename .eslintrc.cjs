15f4c2f405c8eac5a7e00a238fd1821ca1a1f75f
jeong <ibory1220@naver.com>
2023-06-24 08:18:25 +0100

    Project Set up

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb-base',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'react-refresh/only-export-components': 'warn',
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="flex text-blue-300 justify-center">hihi</p>
+      </div>
+    </>
+  )
+}
+
+export default App

fa7638a492406a93cb112e786980dc426b4be29d
tmair32 <tmair32@gmail.com>
2023-06-25 15:50:49 +0900

    Design: All Page Layout #5
    * Intro 페이지 레이아웃 구성
    * Write Letter 페이지 레이아웃 구성
    * View Letter 페이지 레이아웃 구성
    * Send Letter 페이지 레이아웃 구성

--- src/pages/ViewLetter/index.tsx
+++ src/pages/ViewLetter/index.tsx
@@ -1,3 +1,9 @@
-const ViewLetter = () => <div></div>
+const ViewLetter = () => (
+  <section className="w-full h-full grid grid-rows-1 grid-cols-1 items-center justify-center">
+    <div className="w-full h-[50%] min-h-[12.5rem] bg-sky-300">
+      Letter Min-h-200px
+    </div>
+  </section>
+)
 
 export default ViewLetter

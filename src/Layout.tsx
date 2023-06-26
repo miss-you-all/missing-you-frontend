931a194436a807848086b0975d546551430b0262
0uizi0 <0uizi0@naver.com>
2023-06-26 17:17:48 +0900

    Design: Fix CSS errors related to Swiper features

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = () => (
   <>
     <Header />
     <main className="container grid mx-auto grid-cols-4 xl:grid-cols-12 md:grid-cols-8 gap-x-5 h-[calc(100vh-3.5rem)]">
-      <section className="col-span-full md:col-span-4 xl:col-start-5 md:col-start-3 bg-blue-50 h-full p-7">
+      <section className="col-span-full md:col-span-4 xl:col-start-5 md:col-start-3 bg-blue-50 h-full p-7 ">
         <Outlet />
       </section>
     </main>

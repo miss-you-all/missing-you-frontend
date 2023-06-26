1e29cb0f1c0c0cbd0ebe296c6fa1f56351ad937c
0uizi0 <0uizi0@naver.com>
2023-06-26 17:17:48 +0900

    Design: Fix CSS errors related to Swiper features

--- src/pages/WriteLetter/ImageSwiper.tsx
+++ src/pages/WriteLetter/ImageSwiper.tsx
@@ -9,45 +9,27 @@ function ImageSwiper() {
   const images = [{ src: image1 }, { src: image2 }, { src: image3 }]
 
   return (
-    <>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        loop={true}
-        navigation={true}
-        modules={[Navigation]}
-        className="mySwiper"
-      >
-        {images.map((image, idx) => (
-          <SwiperSlide key={idx}>
-            <img src={image.src} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </>
+    <div className="swiper-container z-0">
+      <div className="swiper-wrapper">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={0}
+          loop={true}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          {images.map((image, idx) => (
+            <SwiperSlide key={idx} className="swiper-slide">
+              <div className="image-container">
+                <img src={image.src} />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
   )
 }
 
 export default ImageSwiper
-
-//   const images = [
-//     { src: '../../assets/imgs/taeyang1.jpg' },
-//     { src: '../../assets/imgs/taeyang2.jpg' },
-//     { src: '../../assets/imgs/taeyang3.jpg' },
-//   ]
-
-//   const images = [
-//     { src: require('../../assets/imgs/taeyang1.jpg').default },
-//     { src: require('../../assets/imgs/taeyang2.jpg').default },
-//     { src: require('../../assets/imgs/taeyang3.jpg').default },
-//   ]
-
-//    <SwiperSlide>
-//   <img src={image1} />
-// </SwiperSlide>
-// <SwiperSlide>
-//   <img src={image2} />
-// </SwiperSlide>
-// <SwiperSlide>
-//   <img src={image3} />
-// </SwiperSlide>

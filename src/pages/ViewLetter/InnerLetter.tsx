dbac56ee16c2bc58945e629a6dc61760aba3c160
박 은정 <scpark6021@gmail.com>
2023-06-26 17:20:00 +0900

    Feat: add inner letter #9

--- src/pages/ViewLetter/InnerLetter.tsx
+++ src/pages/ViewLetter/InnerLetter.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import './OpenLetter.css'
 
-const InnerLetter: React.FC = () => {
-  ;<div className="paper-content">
-    <h1>미리보기 컴포넌트</h1>
-    <h1>넣어버리기</h1>
-  </div>
+interface InnerLetterProps {
+  isVisible: boolean
+}
+
+const InnerLetter: React.FC<InnerLetterProps> = ({ isVisible }) => {
+  const res = {
+    recipient: '여러분',
+    sender: '코코',
+    image: '../lookAtMe.jpg',
+    text: 'TS 어렵다. 그리고 졸립다. 태양!! 나만 바라봐!',
+  }
+
+  return (
+    <div className="relative z-99 h-auto bg-white p-5 col-span-6 col-start-2 lg:col-span-4 lg:col-start-5">
+      <div className="border-black border-solid border-2 bg-white">
+        <div className="h-10 m-4">
+          {isVisible && <div className="float-left">TO {res.recipient}</div>}
+        </div>
+        <div>
+          <img src={res.image} alt="letter-image" />
+        </div>
+        {isVisible && (
+          <div className="flex items-center justify-start p-4 mx-auto">
+            {res.text}
+          </div>
+        )}
+        <div className="h-10 m-4">
+          {isVisible && <div className="float-right">From. {res.sender}</div>}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default InnerLetter
